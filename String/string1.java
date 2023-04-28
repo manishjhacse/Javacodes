7c9e0aeb5f9a56b0de8bb079d9e0fb5a2355a381
manishjhacse <jhamanish.cse25@gmail.com>
2023-04-28 23:01:25 +0530

    first commit

---
+++ String/string1.java
@@ -0,0 +1,11 @@
+public class string1 {
+    public static void main(String[] args) {
+        String s1="Manish";
+        System.out.println(s1);
+        String s2=new String("Manish");
+        System.out.println(s2);
+        char []ch={'M','a','n','i','s','h'};
+        String s3=new String(ch);
+        System.out.println(s3);
+    }
+}
