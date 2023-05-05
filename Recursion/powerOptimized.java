562129d8e1d1e28f9d2bb8270b4f7ea57547f655
manishjhacse <jhamanish.cse25@gmail.com>
2023-05-05 22:19:08 +0530

    first commit

--- Recursion/powerOptimized.java
+++ Recursion/powerOptimized.java
@@ -3,14 +3,12 @@ import java.util.*;
 public class powerOptimized {
     public static int power_of_n(int a, int b) {
         int res = 0, finalres = 0;
-        int mid = 0;
         if (b == 0) {
             return 1;
         } else if (b == 1) {
             return a;
         } else {
-            mid = b / 2;
-            res = power_of_n(a, mid);
+            res = power_of_n(a, b/2);
             finalres = res * res;
             if (b % 2 == 0) {
                 return finalres;
