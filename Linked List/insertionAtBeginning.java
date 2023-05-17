c6d84ebfd3a496e57d6422be71ed04f308f74dda
manishjhacse <jhamanish.cse25@gmail.com>
2023-05-17 14:19:15 +0530

    first commit

---
+++ Linked List/insertionAtBeginning.java
@@ -0,0 +1,36 @@
+public class insertionAtBeginning {
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    public void print_Node()
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    public void InsertAtBeg(int newData)
+    {
+        Node newNode=new Node(newData);
+        newNode.next=head;
+        head=newNode;
+    }
+    public static void main(String[] args) {
+        insertionAtBeginning ll=new insertionAtBeginning();
+        ll.InsertAtBeg(5);
+        ll.InsertAtBeg(2);
+        ll.InsertAtBeg(8);
+        ll.print_Node();
+    }
+    
+}
