af39a6835e7bf9921703db3089573e2a65bb33e1
manishjhacse <jhamanish.cse25@gmail.com>
2023-09-19 13:32:23 +0530

    divide and conquer

--- Divide and Conquer/selectionProcedure.java
+++ Divide and Conquer/selectionProcedure.java
@@ -14,6 +14,8 @@ public class selectionProcedure {
         arr[l] = arr[i];
         arr[i] = temp;
         return i;
+
+        
     }
 
     public static int selection_procedure(int arr[], int l, int h, int k) {
